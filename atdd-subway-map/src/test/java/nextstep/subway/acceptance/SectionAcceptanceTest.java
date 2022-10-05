c39b825141cc6bc30620327eaf41a9326efc9de5
DahamLeee <dlek1011@gmail.com>
2022-10-05 14:42:04 +0900

    refactor: use test fixture - Line

--- atdd-subway-map/src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ atdd-subway-map/src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -27,6 +27,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     private Long 신논현역;
     private Long 정자역;
     private Long 판교역;
+    private Long 신분당선;
 
     @BeforeEach
     void initData() {
@@ -34,21 +35,17 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         신논현역 = 지하철역_생성(StationRequest.from("신논현역")).jsonPath().getLong("id");
         정자역 = 지하철역_생성(StationRequest.from("정자역")).jsonPath().getLong("id");
         판교역 = 지하철역_생성(StationRequest.from("판교역")).jsonPath().getLong("id");
+        신분당선 = 지하철_노선_생성(LineRequest.of("신분당선", "bg-red-600", 강남역, 신논현역, 10))
+                .jsonPath().getLong("id");
     }
 
     /**
-     * Given 지하철 노선을 생성하고
-     * When 지하철 구간을 추가하면
+     * When 지하철 노선에 지하철 구간을 추가하면
      * Then 지하철 노선 조회시 추가 된 지하철 역을 조회할 수 있다.
      */
     @DisplayName("지하철 구간 등록")
     @Test
     void addSection() {
-        // given
-        Long 신분당선 = 지하철_노선_생성(LineRequest.of("신분당선", "bg-red-600", 강남역, 신논현역, 10))
-                .jsonPath()
-                .getLong("id");
-
         // when
         지하철_구간_추가(신분당선, SectionRequest.of(신논현역, 정자역, 15));
 
@@ -65,18 +62,12 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철 노선을 생성하고
      * When 기존 노선의 종점역과 새로운 노선의 상행선이 일치하지 않도록 추가하면
      * Then 지하철 구간 등록은 예외를 발생시킨다.
      */
     @DisplayName("지하철 구간 등록 예외(하행역과 상행역 불일치)")
     @Test
     void addSectionUnmatchedException() {
-        // given
-        Long 신분당선 = 지하철_노선_생성(LineRequest.of("신분당선", "bg-red-600", 강남역, 신논현역, 10))
-                .jsonPath()
-                .getLong("id");
-
         // when
         ExtractableResponse<Response> response = 지하철_구간_추가(신분당선, SectionRequest.of(정자역, 판교역, 15));
 
@@ -86,17 +77,12 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철 노선을 생성하고
-     * When 새로운 노선이 이미 존재하는 노선의 중복되는 역을 추가하면
+     * When 기존 노선에 이미 존재하는 역으로 구간을 추가하면
      * Then 지하철 노선은 정상적으로 추가되지 않는다.
      */
     @DisplayName("지하철 구간 등록 예외(이미 존재하는 역 등록)")
     @Test
     void addSectionAlreadyExistsStationException() {
-        // given
-        Long 신분당선 = 지하철_노선_생성(LineRequest.of("신분당선", "bg-red-600", 강남역, 신논현역, 10))
-                .jsonPath().getLong("id");
-
         // when
         ExtractableResponse<Response> response = 지하철_구간_추가(신분당선, SectionRequest.of(신논현역, 강남역, 20));
 
@@ -113,7 +99,6 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철 노선을 생성하고
      * Given 지하철 구간을 추가하고
      * When 노선의 마지막 구간을 제거하면
      * Then 지하철 노선 조회시 해당 구간 정보는 삭제된다.
@@ -121,10 +106,6 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 구간 삭제")
     @Test
     void deleteSection() {
-        // given
-        Long 신분당선 = 지하철_노선_생성(LineRequest.of("신분당선", "bg-red-600", 강남역, 신논현역, 10))
-                .jsonPath().getLong("id");
-
         // given
         지하철_구간_추가(신분당선, SectionRequest.of(신논현역, 정자역, 20));
 
@@ -145,7 +126,6 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철 노선을 생성하고
      * Given 지하철 구간을 추가하고
      * When 구간에 존재하지 않는 역을 삭제하면
      * Then 지하철 구간 삭제 오류가 발생한다.
@@ -153,10 +133,6 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 구간 삭제 예외(존재하지 않는 역 삭제)")
     @Test
     void deleteSectionWhenNotExistsStation() {
-        // given
-        Long 신분당선 = 지하철_노선_생성(LineRequest.of("신분당선", "bg-red-600", 강남역, 신논현역, 10))
-                .jsonPath().getLong("id");
-
         // given
         지하철_구간_추가(신분당선, SectionRequest.of(신논현역, 정자역, 20));
 
@@ -169,17 +145,12 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철 노선을 생성하고
      * When 지하철 구간을 삭제하면
      * Then 지하철 구간 삭제 오류가 발생한다.
      */
     @DisplayName("지하철 구간 삭제 오류(구간이 1개인 노선)")
     @Test
     void deleteSectionOnlyOneSectionException() {
-        // given
-        Long 신분당선 = 지하철_노선_생성(LineRequest.of("신분당선", "bg-red-600", 강남역, 신논현역, 10))
-                .jsonPath().getLong("id");
-
         // when
         ExtractableResponse<Response> response = 지하철_구간_제거(신분당선, 판교역);
 
